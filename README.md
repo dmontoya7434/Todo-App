22cd259cf684195861fefad5e2d0e7254a147aad
daniel montoya <dmontoya7434@eagle.fgcu.edu>
2020-02-13 02:40:49 -0500

    Created a listener mehtod for the form

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ Building a Todo App using react
 * [X] Show a message from state on page
 * [X] Create a New Todo form
 * [X] Call a function when the form is submitted
-* []
-* []
+* [] Create a string property on the state to store the users input
+* [] Listen for when the input changes and updates the state
 * []
 * []
 * []
--- src/App.js
+++ src/App.js
@@ -8,23 +8,34 @@ class App extends Component {
     //All the state of the application will go in here
     //Data of application goes here
     this.state = {
-      message: 'React Application'
+      message: 'React Application',
+      newTodo: ''
     };
   }
   //Defined method
   formSubmitted(event) {
 
     event.preventDefault(); //Prevents default action
-    console.log('form submitted');
+    console.log(this.state.newTodo);
 
+  }
+
+  //Method that logs the latest inut from the user
+  newTodoChanged(event) {
+    //console.log(event.target.value); //stores the value that is logged into the input
+    //passes an object with the properties that are being passed
+    this.setState({
+      newTodo: event.target.value
+
+    });
   }
   render() {
   return (
     <div className="App">
       <h3> {this.state.message}</h3>
-      <form onSubmit={this.formSubmitted}>
+      <form onSubmit={(event) => this.formSubmitted(event)}>
         <label htmlFor="newTodo">New Todo</label>
-        <input id="newTodo" name="newTodo" />
+        <input onChange={(event) => this.newTodoChanged(event)} id="newTodo" name="newTodo" />
         <button type="submit">Add Todo</button>
       </form>
     </div>
